2adceef21a2aaabe3f002d924707e47a381a66e0
Deivi Robles <drozam24@gmail.com>
2019-07-31 20:59:29 -0500

    Set up logs system

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const requestId = require("express-request-id")();
 
+const logger = require("./config/logger");
 const api = require("./api/v1");
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
+//setup middleware
+app.use(requestId);
+app.use(logger.requests);
 app.use("/api/v1", api);
 app.use("/api", api);
 
@@ -12,16 +17,23 @@ app.use("/api", api);
 
 app.use((req, res, next) => {
   res.status(404);
-  res.json({
-    message: "Resource not found",
+  const message = "Route not found";
+  const statusCode = 404;
+
+  next({
+    message,
+    statusCode,
+    level: "info",
   });
 });
 
 // Error middleware
 
 app.use((err, req, res, next) => {
-  const { message, statusCode = 500 } = err;
+  const { message, statusCode = 500, level = "error" } = err;
+  const logMessage = `${logger.header(req)} ${statusCode} ${message}`;
 
+  logger[level](logMessage);
   res.status(statusCode);
   res.json({
     message,
