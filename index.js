4a4848c0d6b26a125266218f497ce20880d95a8b
Deivi Robles <drozam24@gmail.com>
2019-08-06 18:12:42 -0500

    CRUD for Tasks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./server");
 const config = require("./server/config/");
-const database = require("/.server/database");
+const database = require("./server/database");
 
 database.connect(config.database, {});
 
